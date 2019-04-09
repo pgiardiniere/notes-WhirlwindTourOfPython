b8c13595cd3907dac720faedf0d0b659ad3e7e60
pgiardiniere <pgiardiniere@gmail.com>
2019-04-08 22:32:02 -0400

    fix formatting

--- wt12-generators.py
+++ wt12-generators.py
@@ -3,65 +3,65 @@
 # All comments my own
 
 
-# from itertools import count
-# for i in count():
-#     print(i, end=' ')
-#     if i >= 10: break
+from itertools import count
+for i in count():
+    print(i, end=' ')
+    if i >= 10: break
 
 ### a basic Erosthenes Sieve implementation using a generator
-# print('\nfactor divisible gen print until over 40:')
-# factors = [2, 3, 5, 7]
-# G = (i for i in count() if all(i % n > 0 for n in factors))
-# for val in G:
-#     print(val, end=' ')
-#     if val > 40: break
+print('\nfactor divisible gen print until over 40:')
+factors = [2, 3, 5, 7]
+G = (i for i in count() if all(i % n > 0 for n in factors))
+for val in G:
+    print(val, end=' ')
+    if val > 40: break
 
 #############################################
 ### List comprehensions can be used multiple times...
-# L = [n ** 2 for n in range(12)]
-# for val in L:
-#     print(val, end=' ')
-# print()
+L = [n ** 2 for n in range(12)]
+for val in L:
+    print(val, end=' ')
+print()
 
-# for val in L:
-#     print(val, end=' ')
+for val in L:
+    print(val, end=' ')
 
 ### ... while Generators expire after 1 use
-# G = (n ** 2 for n in range(12))
-# print(list(G))
-# print(list(G))
+G = (n ** 2 for n in range(12))
+print(list(G))
+print(list(G))
 #############################################
 
 #############################################
 ### Generator execution can be stopped then restarted from same point
-# G = (n**2 for n in range(12))
-# for n in G:
-#     print(n, end=' ')
-#     if n > 30: break
+G = (n**2 for n in range(12))
+for n in G:
+    print(n, end=' ')
+    if n > 30: break
 
-# print("\ndoing something else here...")
+print("\ndoing something else here...")
 
-# for n in G:
-#     print(n, end=' ')
+for n in G:
+    print(n, end=' ')
 #############################################
 
 #############################################
 ### With 'yield', we can make generator functions
 ### another layer of abstraction, which assists in repeated use of complex generators
-# L1 = [n ** 2 for n in range(12)]
-# L2 = []
-# for n in range(12):
-#     L2.append(n ** 2)
-# print(L1)
-# print(L2)
+L1 = [n ** 2 for n in range(12)]
+L2 = []
+for n in range(12):
+    L2.append(n ** 2)
+print(L1)
+print(L2)
 
-# G1 = (n ** 2 for n in range(12))
-# def gen():
-#     for n in range(12):
-#         yield n**2
-# G2 = gen()
-# print(*G1)
-# print(*G2)
+G1 = (n ** 2 for n in range(12))
+def gen():
+    for n in range(12):
+        yield n**2
+G2 = gen()
+print(*G1)
+print(*G2)
 
 #############################################
 ### Yield+Gen use-case: prime number generator (revisited)
