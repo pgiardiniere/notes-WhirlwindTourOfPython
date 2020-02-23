11b3e004548c7235c4615c1a89b05d7e4bc78dbc
pgiardiniere <pgiardiniere@gmail.com>
2020-02-23 02:34:41 -0500

    ch15 Strings complete, regex partial

--- 14-modulesPkgs.py
+++ 14-modulesPkgs.py
@@ -1,7 +1,3 @@
-### Attribution of code:
-### A Whirlwind Tour of Python by Jake VanderPlas (O’Reilly). Copyright 2016 O’Reilly Media, Inc., 978-1-491-96465-1.
-# All comments my own
-
 ### Explicit module import
 import math
 print(math.cos(math.pi))
@@ -15,9 +11,9 @@ from math import cos, pi
 print(cos(pi))
 
 ### implicit import of module contents (to local namespace)
-### commented belowto squelch errors
-#   from math import *  
-#   print(sin(pi) ** 2 + cos(pi) ** 2)
+### commented below to squelch errors
+from math import *  
+print(sin(pi) ** 2 + cos(pi) ** 2)
 
     # the above is not a recommended method of import. Per the 'problems' in VSCode console,
     # there are 50 funcs in math module imported to local namespace, of which only 3 are used
--- 15-stringsRegex.py
+++ 15-stringsRegex.py
@@ -1,184 +1,145 @@
-### Attribution of code:
-### A Whirlwind Tour of Python by Jake VanderPlas (O’Reilly). Copyright 2016 O’Reilly Media, Inc., 978-1-491-96465-1.
-# All comments my own
+# Strings and Regex in Python
 
+x = 'string'
+y = "string"
+x == y          # returns True (!)
 
-##################################################
-############ Strings: ############################
-# single quotes and double quotes are functionally equivalent
-# multi-line strings declared with triple-quote - i.e.
-multiline = """
-one
-two
-three
+multiLineStr = """\
+first
+second
+third\
 """
-
-### Some basic String Methods:
-s = "stRiNG"
-
-### methods for Case stuff
-s.upper()
-s.lower()
-s.title()
-s.capitalize()
-s.swapcase()
-
-### methods for add/removal of spaces(or arbitrary characters)
-s.strip()
-s.rstrip()
-s.lstrip()
-# note: def. arg = ' ', so this behaves similar to rstrip/lstrip - only removes from outside edges
-s.strip('0f')
-
-# strip() inverse would be center(), ljust(), rjust()
-s.center(30)
-s.ljust(30)
-s.rjust(30)
-# optionally, can specify character to fill instead of whitespace
-s.center(30, '-')
-# zero-filling, a common use
-s.rjust(30, '0')
-# b/c zero-filling is so common, there is a standalone method  
-s.zfill(6)
-# note that zfill behavior is slightly different, arg specifies TOTAL digits, not total indent. e.g.:
-s = '453'
-s.zfill(6)
+print(multiLineStr)
 
 #########################
-### Finding and replacing substrings
-line = 'the quick brown fox jumped over a lazy dog'
+### Formatting Strings
+#########################
+# Adjust case with      upper(), lower(), capitalize(), title(), swapcase()
+fox = "tHe qUICk bROWn fOx."
 
-# find/index behave identically when it matches the string
-line.find('fox')
-line.index('fox')
+fox.upper()
+fox.lower()
+fox.title()           # Capitalize each  word's first letters
+fox.capitalize()      # Capitalize first word's first letter
+fox.swapcase()
 
-# find/index behave differently when it does NOT match the string
-line.find('dsf')
-# print(line.index('dsf')) # index('dsf') throws 'ValueError: substring not found' where find('dsf') returns -1
+# Removing spaces with  strip(), lstrip(), rstrip()
+line = '        content         '
+print ( line.strip(), '\n' )      # remove whitespace from both ends
 
-# rfind/rindex search beginning at the end of string. Returns absolute index position, not relative to end.
-line.rfind('a')
-line.rindex('a')
-line.find('a')
-line.index('a')
+line.rstrip()               # remove whitespace trailing right
+line.lstrip()               # remove whitespace trailing left
 
-# these do exactly what you would think - returns bool
-line.startswith('fox')
-line.endswith('dog')
+num = "000034"
+num.strip('0')              # remove character '0' from string
 
-# instead of just finding, replace() will insert a new substring
-# note: you can get more precise/flexibility with regexs
-line.replace('brown', 'red')
+# Adding space with     center(), ljust(), rjust()
+line = "content"
+line.center(30)
+
+line.ljust(30)
+line.rjust(30)
+
+'435'.rjust(10, '0')    # both pad with 0s
+'435'.zfill(10)
+
 
 #########################
-### Format strings
+### Finding and Replacing substrings
+#########################
+# find() & rfind() ,  index() / rindex() , replace()
+line = "the quick brown fox jumped over a lazy dog"
+line.find('fox')        # if not found, returns -1          
+line.index('fox')       # if not found, returns ValueError
+
+# rfind() / rindex() are identical, but search for first occurrence from str END
+line.rfind('fox')
+
+line.startswith('fox')      # False
+line.endswith('dog')        # True
+
+# replace() works exactly like you'd expect
+print ( line.replace('brown', 'red') )
+print ( line.replace('o', '--'), '\n')
+# ... but regex's are generally the more flexible approach
+
+#########################
+### Splitting and Partitioning strings
+#########################
+# partition() returns tuple :  (substr pre-split, split point, substr post-split)
+# partitions on First Instance of pattern only.
+print ( line.partition('fox') ) # & rpartition() begins search at str END
+
+# split() returns list : 
+# splits string on ALL instances of pattern, ONLY returning substrings w/o split points
+print ( line.split() )      # default: split on whitespace
+
+# splitline() -- for obvious reasons is similar
+multi = """this is
+a string
+of 3 lines"""
+m = multi.splitlines()
+print(m)
+
+# UNDO a split :: use join(), returns str built from split-value and iterable
+print( ' '.join(['1', '2', '3']) )  # undo split()
+print( "\n".join(m) )               # undo splitlines()
+
+#########################
+### Format Strings
+#########################
+# can use curly braces as escape strings
 pi = 3.14159
-str(pi), type(str(pi))
-pi, type(pi)
+str(pi)         # '3.14159'
+"the value of pi is " + str(pi)     # acceptable
+"the value of pi is {}".format(pi)  # format string mode
 
-# for more complicated formats, you may be tempted to do the following:
-"The value of pi is " + str(pi)
+# format strings with args, insert from list
+"""First sub: {0}. Last sub: {1}""".format('poopy', 'butthole')
 
-# the more flexible way is to use Format Strings
-# they are strs with special markers (curly braces) into which string-formatted values are insterted
-"The value of pi is {}".format(pi)
+# format strings with args, named list items
+s = """First sub: {first}. Last sub: {last}""".format(first="poop", last="butt")
+print(s)
 
-# you can include more information on 'what' apppears in the curly braces. nums refer to index of arg to ins
-"""First letter: {0}. Last letter: {1}.""".format('A', 'Z')
+"pi = {0:3f}".format(pi)    # pi as a string to 3 digits of precision
 
-# for numeric inputs, you can include format codes. e.g. to round & return your float pi to 3 dec places...
-"pi = {0:.3f}".format(pi)
 
 #########################
 ### Regular Expressions
-### regex / re
-
-### additional resources:
-# https://docs.python.org/3/library/re.html
-# https://docs.python.org/3/howto/regex.html
-# http://shop.oreilly.com/product/9780596528126.do 
-
-# duplicating str.split() functionality with regex.split() method
+#########################
+# first, we compile a regular expression. Then, we use it to split a string.
 import re
-regex = re.compile(r'\s')
+regex = re.compile('\s+')   # pylint doesn't like ambiguity, but this is correct
 regex.split(line)
 
 for s in ["     ", "abc  ", "  abc"]:
     if regex.match(s):
-        repr(s), "matches"
+        print(repr(s), "matches")
     else:
-        repr(s), "does not match"
+        print(repr(s), "no match")
 
+# now, we compare regex.search() to str.index()/str.find()
+print ( line.index('fox') )
 
-# working with var line to compare str methods and regex methods. re-assign regex to fox
 regex = re.compile('fox')
 match = regex.search(line)
+print ( match.start() )
 
-# regex.search is similar to str.index() ** str.find()
-line.index('fox')
-match.start()
+# now, we compare regex.sub() to str.replace()
+print ( line.replace('fox', 'bear') )
 
-# regex.sub() is similar to str.replace()
-line.replace('fox', 'bear')
-regex.sub('bear', line)
+print ( regex.sub('BEAR', line) )
 
-### demonstration of regex's comparitive flexibility over str methods
-email = re.compile(r'\w+@\w+\.[a-z]{3}')
-text = "To email Guido, try guido@python.org or the older address guido@google.com."
-print(email.findall(text))
-print(email.sub('--@--.--', text))
 
-### basics of regex syntax
-# exact string matches for simple strings
-regex = re.compile('ion')
-print(regex.findall('Greate Expectations'))
+# Why bother? Regex's are more powerful than their simpler counterparts.
+# parsing (basic) email addresses
+text = "To email guido, try guido@python.org or the older adress guido@google.com"
+email = re.compile('\w+@\w+\.[a-z]{3}')
+email.findall(text)
 
-# all re special characters: . ^ $ * + ? { } [ ] \ | ( )
-# match a special character by escaping with a backslash
-regex = re.compile(r'\$')
-print(regex.findall("the cost is $20"))
+print (email.findall(text))
 
-# the r prefix we have been using indicates a Raw String
-# see the difference in output below to denote it's use. 
-# It specifies to stop use of Python-denoted string special chars. Comes into play when backslashing
-print('a\tb\tc')
-print(r'a\tb\tc')
+# alternatively, hide addresses
+email.sub('--@--.--', text)
 
-# escaping with backslash also turns normal chars into special chars
-regex = re.compile(r'\w\s\w')
-print(regex.findall('the fox is 9 years old'))
-# \d \s \w (and their inverse, capitalized counterparts, \D \S \W) for digits, spaces, alphanumeric words + '_'
-# complete list at https://docs.python.org/3/library/re.html#re-syntax 
-# also see personal notes, 'Regular Expressions.txt' in your technical txt folder
-
-# Square brackets for character groups (in a single index/position)
-regex = re.compile('[aeiou]')
-print(regex.split('consequential'))
-
-# dash specifies a range w/in brackets
-regex = re.compile('[A-Z][0-9]')
-print(regex.findall('1043879, G2, H6'))
-
-# wildcards to match repeated characters
-regex = re.compile(r'\w{3}')
-print(regex.findall('The quick brown fox'))
-
-# + operator to return 1 or more (arbitrarily high) occurrences of preceding character (or character group)
-regex = re.compile(r'\w+')
-print(regex.findall("The quick brown fox"))
-
-# revisiting the initial email example
-email  = re.compile(r'\w+@\w\.[a-z]{3}')
-email2 = re.compile(r'[\w.]+@\w+\.[a-z]{3}')
-print(email2.findall('barack.obama@whitehouse.gov'))
-# this works b/c we added brackets and '.', i.e. :: \w+ > [\w.]+ :: [\w.]+ matches any word OR '.' 1 or more times
-
-# parens indicate groups for extraction, so we avoid pulling '@' and '.'
-email3 = re.compile(r'([\w.]+)@(\w+)\.([a-z]{3})')
-text = 'To email Guido, try guido@python.org or the older address guido@google.com.'
-print(email3.findall(text))
-
-# can name extracted components, so instead of export as ...tuple? we get export as dict
-email4 = re.compile(r'(?P<user>[\w.]+)@(?P<domain>\w+)\.(?P<suffix>[a-z]{3})')
-match = email4.match('guido@python.org')
-print(match.groupdict())
+print ( email.sub('--@--.--', text) )
